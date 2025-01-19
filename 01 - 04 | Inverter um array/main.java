16ae124fc45e9b709a9f1ec410978ff45daf0a74
gomes-tkd <jgomestkd@gmail.com>
2025-01-18 22:45:48 -0300

    Atividade 1 - Inverter array

---
+++ 01 - 04 | Inverter um array/main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Entre com o valor do array: ");
+    int sizeArr = scanner.nextInt();
+    int[] arr = new int[sizeArr];
+
+    readArray(arr, scanner);
+    System.out.println("Array Original:");
+    printArray(arr);
+
+    arr = inverterArray(arr);
+    System.out.println("Array Invertido:");
+    printArray(arr);
+
+    scanner.close();
+  }
+
+  public static void readArray(int[] arr, Scanner sc) {
+    System.out.println("Entre com os valores:");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.printf("  Entre com o valor %d/%d: ", i + 1, arr.length);
+      arr[i] = sc.nextInt();
+    }
+    System.out.println();
+  }
+
+  public static void printArray(int[] arr) {
+    System.out.println("Imprimindo os valores:");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.printf("  Posição %d/%d: %d\n", i, arr.length, arr[i]);
+    }
+    System.out.println();
+  }
+
+  public static int[] inverterArray(int[] arr) {
+    int[] arrInvertido = new int[arr.length];
+
+    for (int i = 0; i < arr.length; i++) {
+      int aux = arr[i];
+      arrInvertido[i] = arr[arr.length - i - 1];
+      // arr[i] = arr[arr.length - i - 1];
+      arr[arr.length - i - 1] = aux;
+    }
+
+    return arrInvertido;
+  }
+
+}
